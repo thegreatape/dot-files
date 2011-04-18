5da79b11e41806f4c4e20c198dd2474894cf15a6
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2011-04-17 23:20:16 -0400

    rename bashrc and add auto-linking script (tip o' the hat to @throughnothing)

---
+++ install.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+# Get all the file, with the ones we don't want to link filtered out
+files = Dir['*'].reject {|f| f =~ /^(.*\.kmmacros|install\.rb|todo\.cfg|README)/i}
+
+# Create an absolute path from our working directory
+files = files.map { |file| File.join( File.dirname(File.expand_path(__FILE__)), file ) } 
+
+home = File.expand_path('~/')
+puts "Linking in #{home}/"
+files.each do |from|
+  next unless (File.file?(from) || File.directory?(from))
+
+  to = File.join(home, '.' + File.basename(from))
+
+  puts " - " + [from, to].join(' -> ')
+  if File.symlink? to
+    FileUtils.rm to
+    puts " ! Target exists... as a symlink, removed"
+  elsif File.exists?(to)
+    puts " ! Target exists... as a normal file/directory, moving to #{File.basename(to)}~... "
+    toto = to + '~'
+
+    if File.exist? toto
+      print "already exists! r)emove, or s)kip? "
+      order = STDIN.gets.chomp
+
+      case order
+      when 'r'
+        puts ' ! Removing... '
+        FileUtils.rm toto
+      when 's'
+        puts ' ! Okay, skipped '
+        next
+      else
+        puts " ! Invalid entry, so skipping"
+        next
+      end
+    end
+
+    FileUtils.mv to, toto
+    puts "Done!"
+  end 
+
+  FileUtils.symlink(from, to)
+end
+
