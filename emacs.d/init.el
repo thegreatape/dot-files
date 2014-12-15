6afbfd62c84d1950603490e659b69dc9ba0341c5
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2014-12-15 06:30:54 -0500

    Add window switching keybindings to evil-mode.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,4 +16,11 @@
 
 (use-package evil
   :ensure t
-  :init (evil-mode t))
+  :init
+  (progn
+    (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+    (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+    (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+    (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
+    (evil-mode t)))
