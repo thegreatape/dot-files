ac763e4aa477483d5d9c406b547e62bbf24159ec
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2015-06-03 21:24:54 -0400

    In-place file renaming.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -62,6 +62,23 @@
 (global-set-key (kbd "M--") 'debiggen-font)
 (set-font my-font-size)
 
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
 ; set up package management with use-package
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -85,7 +102,8 @@
     (evil-leader/set-key "t" 'projectile-find-file)
     (evil-leader/set-key "ag" 'projectile-ag)
     (evil-leader/set-key "pp" 'projectile-switch-project)
-    (evil-leader/set-key "cl" 'evilnc-comment-or-uncomment-lines)))
+    (evil-leader/set-key "cl" 'evilnc-comment-or-uncomment-lines)
+    (evil-leader/set-key "n" 'rename-current-buffer-file)))
 
 (use-package evil
   :ensure t
