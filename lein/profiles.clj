ad00066e7c8a735a2c99ebcb2ea535337b832ec2
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2019-04-08 09:57:24 -0400

    remove slamhound + cider-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,5 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-  :dependencies [[slamhound "1.5.5"]
-                 [spyscope "0.1.3"]
+ {:dependencies [[spyscope "0.1.3"]
                  [redl "0.2.4"]]
   :injections [(require 'spyscope.core)
                (require '[redl complete core])]}}
